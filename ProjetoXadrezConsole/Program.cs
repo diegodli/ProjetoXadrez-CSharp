6317432f81b85d2f5694c6ccdf2fa5d1a77534d9
Diego de Lima <diegolimax555@gmail.com>
2024-05-01 14:24:54 -0300

    Controlando as peças capturadas

--- ProjetoXadrezConsole/Program.cs
+++ ProjetoXadrezConsole/Program.cs
@@ -16,10 +16,7 @@ namespace ProjetoXadrezConsole
                     try
                     {
                         Console.Clear();
-                        Tela.ImprimirTabuleiro(partida.Tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.Turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+                        Tela.ImprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
